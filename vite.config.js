6a3365885e0898a717e09fd8629e80487acc6b20
emanesmail888 <negma.oula@yahoo.com>
2026-07-31 13:18:41 +0300

    Update vite for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,6 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     chunkSizeWarningLimit: 1000,
-    rollupOptions: {
-      output: {
-      // manualChunks(id) {
-      //   if (id.includes('node_modules')) {
-      //     return 'vendor';
-      //   }
-      // }
-      }
-    }
+    
   }
 })
