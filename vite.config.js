dae42864ae7d55553108648f741532106cd98890
emanesmail888 <negma.oula@yahoo.com>
2026-08-01 11:14:17 +0300

    laravel  commit5

--- vite.config.js
+++ vite.config.js
@@ -8,15 +8,5 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-    minify: 'terser',
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['vue', 'vuex', 'axios'],
-          // Add more heavy libs if needed
-        }
-      }
-    }
-  }
+    
 });
