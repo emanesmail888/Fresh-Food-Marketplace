995448f5b946735e47636c689f1f5326562b3723
emanesmail888 <negma.oula@yahoo.com>
2026-08-08 02:02:56 +0300

    deploy to railway11

--- database/migrations/2026_01_12_023919_create_products_table.php
+++ database/migrations/2026_01_12_023919_create_products_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 2000);
-            $table->string('slug', 2000);
-            $table->string('title_ar', 2000);
+            $table->string('title', 255);
+            $table->string('slug', 255);
+            $table->string('title_ar', 255);
             $table->string('image')->nullable();
             $table->text('images')->nullable();
             $table->longText('description')->nullable();
